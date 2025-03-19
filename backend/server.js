888e3d96b2788608473d97256f9dab696c0ae318
keshavcoded <keshav.shri06@gmail.com>
2025-03-19 19:37:09 +0530

    mod: product route

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./configs/db.js";
 import cookieParser from "cookie-parser";
 import { ENV_VARS } from "./configs/envVars.js";
+import productRoutes from "./routes/product.route.js";
 
 const app = express();
 
@@ -14,6 +15,7 @@ dotenv.config();
 const PORT = ENV_VARS.PORT || 3000;
 
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/products", productRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
