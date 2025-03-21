e0752d8bb332c1585771744114a80d5044c1d3fa
keshavcoded <keshav.shri06@gmail.com>
2025-03-21 14:37:07 +0530

    add : create delete and featured products route

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,4 +1,6 @@
+import { redis } from "../configs/redis.js";
 import Product from "../models/product.model.js";
+import cloudinary from "../configs/cloudinary.js";
 
 export const getAllProducts = async (req, res) => {
   try {
@@ -9,6 +11,91 @@ export const getAllProducts = async (req, res) => {
     });
   } catch (error) {
     console.log("Error in getAllProducts controller ", error.message);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+};
+
+export const createProduct = async (req, res) => {
+  try {
+    const { name, description, price, image, category } = req.body;
+    let cloudinaryResponse = null;
+
+    if (image) {
+      cloudinaryResponse = await cloudinary.uploader.upload(image, {
+        folder: "products",
+      });
+    }
+    const product = await Product.create({
+      name,
+      description,
+      price,
+      image: cloudinaryResponse?.secure_url
+        ? cloudinaryResponse.secure_url
+        : "",
+      category,
+    });
+    return res
+      .status(201)
+      .json({ success: true, message: "Product created", product: product });
+  } catch (error) {
+    console.log("Error in create product controller : ", error.message);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal server error" });
+  }
+};
+
+export const deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params.id;
+    const product = await Product.findById(id);
+    if (!product) {
+      return res
+        .status(404)
+        .json({ success: false, message: " Product not found" });
+    }
+
+    if (product.image) {
+      const publicID = product.image.split("/").pop().split(".")[0];
+      try {
+        await cloudinary.uploader.destroy(`products/${publicID}`);
+        console.log("Product deleted from Cloudinary ID: ", publicID);
+      } catch (error) {
+        console("Error in deleting image from cluodinary", error.message);
+      }
+    }
+
+    await Product.findByIdAndDelete(id);
+
+    return res
+      .status(200)
+      .json({ success: true, message: "Product deleted succesfully" });
+  } catch (error) {
+    console.log("Error in delete product controller : ", error.message);
+    return res
+      .status(500)
+      .json({ sucess: false, message: "Internal server error" });
+  }
+};
+
+export const getFeaturedProducts = async (req, res) => {
+  try {
+    let featuredProducts = await redis.get("featured_products");
+    if (featuredProducts) {
+      return res.json(JSON.parse(featuredProducts));
+    } else {
+      featuredProducts = await Product.find({ isFeatured: true }).lean();
+      if (!featuredProducts) {
+        return res
+          .status(404)
+          .json({ success: false, message: "No featured products found" });
+      } else {
+        await redis.set("featured_products", JSON.stringify(featuredProducts));
+        return res.json({ success: true, featuredProducts: featuredProducts });
+      }
+    }
+  } catch (error) {
+    console.log("Error in featured products controller : ", error.message);
     res.status(500).json({ success: false, message: "Internal server error" });
   }
 };
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,9 +1,17 @@
 import express from "express";
-import { getAllProducts } from "../controllers/product.controller.js";
+import {
+  createProduct,
+  deleteProduct,
+  getAllProducts,
+  getFeaturedProducts,
+} from "../controllers/product.controller.js";
 import { checkAdmin, checkAuth } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.get("/", checkAuth, checkAdmin, getAllProducts);
+router.post("/", checkAuth, checkAdmin, createProduct);
+router.delete("/", checkAuth, checkAdmin, deleteProduct);
+router.get("/featured", getFeaturedProducts);
 
 export default router;
