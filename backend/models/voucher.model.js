c9d42d634163efa06f9992f2cea2c69212f046dd
keshavcoded <keshav.shri06@gmail.com>
2025-03-22 23:38:19 +0530

    add : added voucher schema

---
+++ backend/models/voucher.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const voucherSchema = new mongoose.Schema(
+  {
+    code: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    expiryDate: {
+      type: Date,
+      required: true,
+    },
+    isActive: {
+      type: Boolean,
+      required: true,
+    },
+    discountPercent: {
+      type: Number,
+      required: true,
+      min: 0,
+      max: 100,
+    },
+    userID: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Voucher = mongoose.model("Voucher", voucherSchema);
+
+export default Voucher;
