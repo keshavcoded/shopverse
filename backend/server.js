3220d03fa7d813b8973ee1955ff4fa88926e375a
keshavcoded <keshav.shri06@gmail.com>
2025-03-25 23:25:57 +0530

    init: payment routes

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import { ENV_VARS } from "./configs/envVars.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
 import voucherRoutes from "./routes/voucher.route.js";
+import paymentRoutes from "./routes/payment.route.js";
 
 const app = express();
 
@@ -20,6 +21,7 @@ app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/products", productRoutes);
 app.use("/api/v1/cart", cartRoutes);
 app.use("/api/v1/vouchers", voucherRoutes);
+app.use("/api/v1/payments", paymentRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
