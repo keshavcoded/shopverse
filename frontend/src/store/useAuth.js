c86c545d0af86dfd7711083be83ede0ebf962d88
keshavcoded <keshav.shri06@gmail.com>
2025-04-02 00:11:43 +0530

    add : user auth store

---
+++ frontend/src/store/useAuth.js
@@ -0,0 +1,96 @@
+import { create } from "zustand";
+import { toast } from "react-hot-toast";
+import axiosInstance from "../utils/axios";
+
+export const useAuthStore = create((set) => {
+  return {
+    user: null,
+    isSigningUp: false,
+    isCheckingAuth: true,
+    isSigningOut: false,
+    isSigningIn: false,
+
+    authUserCheck: async () => {
+      set({ isCheckingAuth: true });
+      try {
+        const res = await axiosInstance.get("/auth/user-profile");
+        set({ user: res.data, isCheckingAuth: false });
+      } catch (error) {
+        console.log(error.message);
+        set({ isCheckingAuth: false, user: null });
+      }
+    },
+
+    signup: async ({ name, email, password, confirmPassword }) => {
+      set({ isSigningUp: true });
+
+      if (password !== confirmPassword) {
+        set({ isSigningUp: false });
+        return toast.error("Passwords do not match");
+      }
+      try {
+        const res = await axiosInstance.post("/auth/signup", {
+          name,
+          email,
+          password,
+        });
+        set({ user: res.data, isSigningUp: false });
+        return toast.success("Signed up successfully");
+      } catch (error) {
+        if (!Array.isArray(error.response?.data?.errors)) {
+          toast.error(error.response?.data?.message || "Signup failed");
+        } else {
+          /* already shown error marking */
+          const shownFields = new Set();
+
+          error.response.data.errors.forEach((err) => {
+            if (!shownFields.has(err.field)) {
+              toast.error(err.message);
+              shownFields.add(err.field);
+            }
+          });
+        }
+        set({ isSigningUp: false, user: null });
+      }
+    },
+
+    signin: async ({ email, password }) => {
+      set({ isSigningIn: true });
+      try {
+        const res = await axiosInstance.post("/auth/signin", {
+          email,
+          password,
+        });
+        set({ user: res.data, isSigningIn: false });
+        return toast.success("Signed in successfully");
+      } catch (error) {
+        if (!Array.isArray(error.response?.data?.errors)) {
+          toast.error(error.response?.data?.message || "Signup failed");
+        } else {
+          /* already shown error marking */
+          const shownFields = new Set();
+
+          error.response.data.errors.forEach((err) => {
+            if (!shownFields.has(err.field)) {
+              toast.error(err.message);
+              shownFields.add(err.field);
+            }
+          });
+        }
+        set({ isSigningIn: false, user: null });
+      }
+    },
+
+    signout: async () => {
+      set({ isSigningOut: true });
+      try {
+        await axiosInstance.post("/auth/signout");
+        set({ user: null, isSigningOut: false });
+        toast.success("Logged out successfully");
+      } catch (error) {
+        toast.error(error.response?.data?.message || "Error while logging out");
+        set({ isSigningOut: true });
+      }
+    },
+  };
+});
