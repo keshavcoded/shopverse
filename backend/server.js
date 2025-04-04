6a600f2432bbad3fe10950e5fd8c4a86485d6acc
keshavcoded <keshav.shri06@gmail.com>
2025-04-05 01:23:16 +0530

    fix: set limit to 5MB for image uploads

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ import analyticsRoutes from "./routes/analytics.route.js";
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
 
 dotenv.config();
