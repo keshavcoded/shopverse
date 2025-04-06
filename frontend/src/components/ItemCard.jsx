46d40a89540a6cd466e496a5a9723dec4d232ea7
keshavcoded <keshav.shri06@gmail.com>
2025-04-07 00:54:02 +0530

    ui: Item category card

---
+++ frontend/src/components/ItemCard.jsx
@@ -0,0 +1,54 @@
+import { ShoppingCart } from "lucide-react";
+import toast from "react-hot-toast";
+import { useAuthStore } from "../store/useAuth";
+import { useNavigate } from "react-router-dom";
+
+const ItemCard = ({ product }) => {
+  const { user } = useAuthStore();
+  const navigate = useNavigate();
+
+  const handleCartClick = () => {
+    if (!user) {
+      toast("Please login to continue shopping", {
+        icon: "𝒊",
+      });
+      navigate("/signin");
+    } else {
+      toast.success("Added to cart");
+    }
+  };
+  return (
+    <div className="flex flex-col w-full max-w-sm mx-auto overflow-hidden rounded-2xl border border-gray-200 bg-white shadow-md hover:shadow-2xl transition-shadow duration-300">
+      <div className="w-full bg-gray-100 flex items-center justify-center p-4 aspect-[4/3]">
+        <img
+          src={product.image}
+          alt={product.name}
+          className="max-h-full max-w-full object-contain"
+        />
+      </div>
+
+      {/* Product content */}
+      <div className="flex flex-col justify-between p-5 h-full">
+        <h5 className="text-lg font-semibold tracking-tight text-gray-800 line-clamp-2">
+          {product.name}
+        </h5>
+
+        <div className="mt-3 flex items-center justify-between">
+          <span className="text-xl font-bold text-gray-800">
+            ${product.price}
+          </span>
+        </div>
+
+        <button
+          className="mt-5 flex items-center justify-center rounded-lg bg-gray-200 px-5 py-2.5 text-sm font-medium text-gray-800 hover:bg-gray-300 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-gray-400 cursor-pointer"
+          onClick={handleCartClick}
+        >
+          <ShoppingCart size={20} className="mr-2" />
+          Add to Cart
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ItemCard;
--- frontend/src/pages/Category.jsx
+++ frontend/src/pages/Category.jsx
@@ -1,6 +1,9 @@
 import { useEffect } from "react";
 import { useProductStore } from "../store/useProduct";
 import { useParams } from "react-router-dom";
+// eslint-disable-next-line no-unused-vars
+import { motion } from "framer-motion";
+import ItemCard from "../components/ItemCard";
 
 const Category = () => {
   const { fetchProductsByCategory, products } = useProductStore();
@@ -12,7 +15,36 @@ const Category = () => {
 
   console.log("Products : ", products);
 
-  return <div>Category Page</div>;
+  return (
+    <div className="min-h-screen">
+      <div className="relative z-10 max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
+        <motion.div
+          className="text-center text-2xl sm:text-4xl font-bold text-gray-700 mb-8"
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8 }}
+        >
+          {category}
+        </motion.div>
+        <motion.div
+          className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 justify-items-center"
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, delay: 0.2 }}
+        >
+          {products?.length === 0 && (
+            <h2 className="text-3xl font-bold text-gray-700 text-center col-span-full">
+              No products found.
+            </h2>
+          )}
+
+          {products?.map((product) => (
+            <ItemCard key={product._id} product={product} />
+          ))}
+        </motion.div>
+      </div>
+    </div>
+  );
 };
 
 export default Category;
