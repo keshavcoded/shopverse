5ed3f4c311676cc898848cce2747a73a5e5a70c4
keshavcoded <keshav.shri06@gmail.com>
2025-04-12 17:39:24 +0530

    util: stripe instance function

---
+++ frontend/src/utils/stripe.js
@@ -0,0 +1,5 @@
+import { loadStripe } from "@stripe/stripe-js";
+
+export const stripePromise = loadStripe(
+  "pk_test_51R7tHcEK9KYNsg4j9xVlbjyJc0n65Rh30oObrRyQWQ8AtX1wnnPfz1PAqmraoEIePdfzcJtsYbMgRU9JfBm3DENT00UGgpJtW6"
+);
