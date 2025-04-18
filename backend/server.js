ca78cf737c97fe88b0a145dd1462e19227904b8b
keshavcoded <keshav.shri06@gmail.com>
2025-04-18 16:31:59 +0530

    changes for production

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ import cartRoutes from "./routes/cart.route.js";
 import voucherRoutes from "./routes/voucher.route.js";
 import paymentRoutes from "./routes/payment.route.js";
 import analyticsRoutes from "./routes/analytics.route.js";
+import path from "path";
 
 const app = express();
 
@@ -18,12 +19,22 @@ app.use(cookieParser());
 dotenv.config();
 const PORT = ENV_VARS.PORT || 3000;
 
+const __dirname = path.resolve();
+
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/products", productRoutes);
 app.use("/api/v1/cart", cartRoutes);
 app.use("/api/v1/vouchers", voucherRoutes);
 app.use("/api/v1/payments", paymentRoutes);
 app.use("/api/v1/analytics", analyticsRoutes);
+
+if (ENV_VARS.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
